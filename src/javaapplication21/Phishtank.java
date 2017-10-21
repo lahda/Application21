d148998b57b86218dae302a8a32675517cb62fd7
Alph <Alph@Alphonsine>
2017-10-21 16:17:38 +0200

    heo

---
+++ src/javaapplication21/Phishtank.java
@@ -0,0 +1,32 @@
+
+//my key phishtank:47998607b92b95d923e7084aa8dfd59b2db319ea01b8b06314c1ec032d5368e6
+package javaapplication21;
+
+/**
+ *
+ * @author Alph
+ */
+pub array(
+      array(
+            'phish_id' => 123456,
+            'url' => 'http://www.example.com/',
+            'phish_detail_url' => 'http://www.phishtank.com/phish_detail.php?phish_id=123456',
+            'submission_time' => '2009-06-19T15:15:47+00:00',
+            'verified' => 'yes',
+            'verification_time' => '2009-06-19T15:37:31+00:00',
+            'online' => 'yes',
+            'target' => '1st National Example Bank',
+            'details' => array(
+                               array(
+                                     'ip_address' => '1.2.3.4',
+                                     'cidr_block' => '1.2.3.0/24',
+                                     'announcing_network' => '1234',
+                                     'rir' => 'arin',
+                                     'detail_time' => '2006-10-01T02:30:54+00:00'                         
+                                    )
+                               )
+           )
+)lic class Phishtank {
+    
+    
+}
